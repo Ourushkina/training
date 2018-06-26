bbb3cba439bf8357027c5b5f17ab98a8c55cb38c
olga <Olechka56741@gmail.com>
2018-06-26 17:02:20 +0300

    Создан удаленный репозиторий для обучения2

--- geometry.py
+++ geometry.py
@@ -1,9 +1,19 @@
 from geom2d import *
 
-l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
+
+l = []
 
 
-l2 = sorted(l1, key=lambda p: p.distance(Point(0, 0)))
+for i in range(-5, 6):
+    l.append(Point(i, i*i))
 
-print(l1)
+
+l2 = []
+
+
+for el in l:
+    l2.append(Point(el.x, -el.y))
+
+
+print(l)
 print(l2)
